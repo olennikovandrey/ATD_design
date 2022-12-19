f375beb37165addd479b77a2443dd091b5899c2b
olennikovandrey <olennikovandrey@mail.ru>
2022-12-19 17:09:49 +0300

    initial

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  const navs = ["Main", "Services", "Contacts"];
+
+  return (
+    <header>
+      <div>
+        <span className="logo">ATD Design and Consultancy FZ-LLC</span>
+        <div className="navs-wrapper">
+        {
+          navs.map(item =>
+            <nav>{ item }</nav>
+          )
+        }
+      </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
